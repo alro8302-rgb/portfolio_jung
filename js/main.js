c8cfd6d0de4fe7c909129635f026e032da9b48b1
JUNGMIGYOUNG <alro8302@gamil.com>
2026-06-09 13:13:47 +0900

    헤더, 비주얼 작업

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// 햄버거 바 클릭시
+const menuBtn = document.querySelector(".menu-btn");
+const mobileMenu = document.querySelector(".mobile-menu");
+menuBtn.addEventListener("click", function () {
+    // console.log("click");
+    // x변경
+    menuBtn.classList.toggle("active");
+    // 모바일 메뉴가 나타남
+    mobileMenu.classList.toggle("active");
+});
+// 화면 크기 변경시 모바일 메뉴 닫기
+window.addEventListener("resize", function () {
+    // 768px보다 커지면
+    if (window.innerWidth > 768) {
+        // 모바일메뉴 닫기
+        mobileMenu.classList.remove("active");
+        // 햄버거 버튼
+        menuBtn.classList.remove("active");
+    }
+});
+// 모바일메뉴 링크 클릭시
+const mbMenuLink = document.querySelectorAll(".mobile-menu a");
+// console.log(mbMenuLink);
+// 모바일메뉴 a 각각 꺼낸다
+mbMenuLink.forEach((link) => {
+    // console.log(link);
+    link.addEventListener("click", () => {
+        // 모바일메뉴 닫기
+        mobileMenu.classList.remove("active");
+        // 햄버거메뉴 X 원래대로
+        menuBtn.classList.remove("active");
+    });
+});
